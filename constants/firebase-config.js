29ff208604361f84c04fff4cdc2a662047b33411
Maxime Cortes <maxime.cortes54@gmail.com>
2025-05-11 15:45:03 +0200

    test

--- constants/firebase-config.js
+++ constants/firebase-config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDIVqtaSgmmCS8Ro6FKVSznViK0eR04fJQ",
   authDomain: "challengeme-d7fef.firebaseapp.com",
   projectId: "challengeme-d7fef",
-  storageBucket: "challengeme-d7fef.firebasestorage.app", // Mise à jour ici
+  storageBucket: "challengeme-d7fef.firebasestorage.app",
   messagingSenderId: "344780684076",
   appId: "1:344780684076:web:8f1208a81ec9fbe25dca89",
   measurementId: "G-71TM15XWT8",
